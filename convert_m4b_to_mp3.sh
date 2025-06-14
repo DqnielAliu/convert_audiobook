6bf1a7c4887afa7ab785bb0e7a50a214bb68b709
Daniel Aliu <contact.danielaliu@gmail.com>
2025-06-14 15:40:10 +0100

    minor changes: Parameter parsing, dynamic bitrate, attempting to handle directories

--- convert_m4b_to_mp3.sh
+++ convert_m4b_to_mp3.sh
@@ -53,53 +53,72 @@ Borrowed from https://apprenticealf.wordpress.com/
 "
 
 declare -i DRYRUN=0
-declare -r FFMPEG_LOGLEVEL="-loglevel info"
+declare -r FFMPEG_LOGLEVEL="-loglevel error"
 declare -a INPUT_FILES=()
 declare -i OUTPUT_MP3=0
-declare -- OUTPUT_BITRATE="192k"
+declare -i CHAPTERED=1
+# declare -- _BITRATE=
 
-for arg in "$@"
-do
-    case $arg in
-    --dryrun)
-        DRYRUN=1
-        shift
-        ;;
-    -h|--help)
-        echo -e "$HELP"
-        exit 1
-        shift
-        ;;
-    --mp3)
-        OUTPUT_MP3=1
-        OUTPUT_EXT="mp3"
-        OUTPUT_CODEC="libmp3lame"
-        shift
-        ;;
-    --bitrate=*)
-        OUTPUT_BITRATE=${arg#*=}
-        shift
-        ;;
-    *)
-        if [[ -f "$arg" ]]; then
-            if [[ "$arg" == *".m4b" ]]; then
-                OUTPUT_MP3=1
-                INPUT_FILES+=( "$arg" )
-            fi
-        elif [[ -d "$arg" ]]; then
-            # untested with directories ... 
-            # or add recusion here later
-            # (mwhahahahaa)
-            INPUT_FILES=$(find "$1" -type f -name "*.m4b")
-            echo "- Ignoring directory '"$arg"'"
-        else
-            echo -e "Unknown argument '"$arg"'.\n Try './${0##*/} --help' for usage.\n"
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --dryrun)
+            DRYRUN=1
+            shift
+            ;;
+        -h|--help)
+            echo -e "$HELP"
             exit 1
-        fi
-        shift
-        ;;
+            ;;
+        -c|--chaptered)
+            CHAPTERED=1
+            shift
+            ;;
+        -nc|--nochaptered)
+            CHAPTERED=0
+            shift
+            ;;
+        --format)
+            FORMAT="$2"
+            shift 2
+            ;;
+        --mp3)
+            OUTPUT_MP3=1
+            OUTPUT_EXT="mp3"
+            OUTPUT_CODEC="libmp3lame"
+            OUTPUT_CONTAINER="mp3"
+            shift
+            ;;
+        --bitrate)
+            _BITRATE="$2"
+            shift 2
+            ;;
+        *)
+            if [[ -f "$1" ]]; then
+                if [[ "$1" == *.m4b ]]; then
+                    OUTPUT_MP3=1
+                    INPUT_FILES+=( "$1" )
+                fi
+            elif [[ -d "$1" ]]; then
+                # untested with directories ... 
+                # or add recusion here later
+                # (mwhahahahaa)
+                # echo $arg;
+                # while IFS= read -r -d '' file; do
+                #     INPUT_FILES+=( "$file" )
+                # done < <(find "$arg" -type f -name "*.m4b" -print0)
+                for item in "$1"/*.m4b; do
+                    INPUT_FILES+=( "$item" )
+                done
+                echo "- Ignoring directory '$1'"
+            else
+                echo -e "Unknown argument '$1'.\nTry './${0##*/} --help' for usage.\n"
+                exit 1
+            fi
+            shift
+            ;;
     esac
 done
+
 
 # no output set?
 if [[ "$OUTPUT_MP3" == 0 ]]; then
@@ -113,11 +132,24 @@ if [[ ${#INPUT_FILES[@]} == 0 ]]; then
     exit 1
 fi
 
+# For testing purpose
+for file in "${INPUT_FILES[@]}"
+do
+    echo "$file"
+done
+
 #Vroom Vrooom
 for INPUT_FILE in "${INPUT_FILES[@]}"; do
     SPLIT=$SECONDS
     # temporary folder.
     WORKPATH=$(mktemp -d -t ${0##*/}-XXXXXXXXXX)
+    
+    if [[ -n "$_BITRATE" ]]; then
+        BITRATE_KBPS=${_BITRATE}
+    else
+        _BITRATE_BPS=$(ffprobe -v error -show_entries format=bit_rate -of default=noprint_wrappers=1:nokey=1 "$INPUT_FILE")
+        BITRATE_KBPS=$(echo "scale=0; ($_BITRATE_BPS + 500) / 1000" | bc) # Add 500 for proper rounding
+    fi
 
     # Book info
     BOOKTITLE=$(ffprobe -v quiet -show_format "$INPUT_FILE" | grep "TAG:title" | cut -d"=" -f2 | tr -d '"')
@@ -166,7 +198,7 @@ for INPUT_FILE in "${INPUT_FILES[@]}"; do
     JOBCOVER="$WORKPATH/jobs_covers.sh"
     echo "#!/bin/bash" | tee "$JOBCOVER" 1> /dev/null
     chmod +x "$JOBCOVER"
-    ffmpeg $FFMPEG_LOGLEVEL -y -i "$INPUT_FILE" "$COVERIMG"
+    ffmpeg $FFMPEG_LOGLEVEL -y -i "$INPUT_FILE" -frames:v 1 "$COVERIMG"
 
     # M4B (direct copy with all metadata sans encryption - cover retained from original file)
     # use this as the source file from here on out
@@ -174,7 +206,7 @@ for INPUT_FILE in "${INPUT_FILES[@]}"; do
     echo "- Creating \"$FSBOOKTITLE.m4b\""
     if [[ "$DRYRUN" == 0 ]]; then
         DRMFREE="$WORKPATH/m4b/$FSBOOKTITLE.m4b"
-        ffmpeg -loglevel error -stats -i "$INPUT_FILE" -c copy "$DRMFREE"
+        ffmpeg -loglevel error -stats -i "$INPUT_FILE" -map 0:a -c copy "$DRMFREE"
     fi
     # Dryrun referances initial file
     if [[ "$DRYRUN" == 1 ]]; then
@@ -186,13 +218,13 @@ for INPUT_FILE in "${INPUT_FILES[@]}"; do
     echo "#!/bin/bash" | tee "$JOBENCODER" 1> /dev/null
     chmod +x "$JOBENCODER"
     
-    # MP3 (one track per chapter, 64kbps, metadata and playlist)
-    if [[ "$OUTPUT_MP3" == 1 ]]; then
+    # MP3 (one track per chapter, metadata and playlist)
+    if [[ "$CHAPTERED" == 1 && -z ${#ID[@]} ]]; then
         echo "- Preparing $OUTPUT_EXT Encoding Jobs"
         mkdir "$WORKPATH/$OUTPUT_EXT"
-        PLAYLIST="$WORKPATH/$OUTPUT_EXT/00. $FSBOOKTITLE ($FSAUTHOR $YEAR).m3u"
+        PLAYLIST="$WORKPATH/$OUTPUT_EXT/$FSBOOKTITLE ($FSAUTHOR $YEAR).m3u"
         echo -e "#EXTM3U\n#EXTENC: UTF-8\n#EXTGENRE:$GENRE\n#EXTART:$AUTHOR\n#PLAYLIST:$BOOKTITLE ($AUTHOR $YEAR)" | tee "$PLAYLIST" 1> /dev/null
-
+        
         for i in ${!ID[@]}
         do
             let TRACKNO=$i+1
@@ -201,11 +233,11 @@ for INPUT_FILE in "${INPUT_FILES[@]}"; do
             # mp3 encoder job
             OUTPUT_ENCODE="_$TRACKNO.$OUTPUT_EXT"
             OUTPUT_FINAL="$(printf "%02d" $TRACKNO). $FSBOOKTITLE - ${TITLE[$i]}.$OUTPUT_EXT"
-            COMMAND="echo \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\" && \
-                ffmpeg $FFMPEG_LOGLEVEL -i \"${DRMFREE/"\$"/"\\\$"}\" -vn -c $OUTPUT_CODEC \
+            COMMAND="set -x;echo \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\" && \
+                ffmpeg $FFMPEG_LOGLEVEL -i \"${DRMFREE/"\$"/"\\\$"}\" -vn \
                 -ss ${START_TIME[$i]} -to ${END_TIME[$i]} \
                 -map_chapters -1 \
-                -id3v2_version 3 \
+                -id3v2_version 4 \
                 -metadata title=\"${TITLE[$i]}\" \
                 -metadata track=\"$TRACKNO/${#ID[@]}\" \
                 -metadata album=\"$BOOKTITLE\" \
@@ -217,22 +249,27 @@ for INPUT_FILE in "${INPUT_FILES[@]}"; do
                 -metadata album-sort=\"$ALBUM_SORT\"
                 -metadata artist-sort=\"$ARTIST_SORT\"
                 -codec:a $OUTPUT_CODEC \
-                -b:a $OUTPUT_BITRATE \
+                -b:a \"${BITRATE_KBPS}k\" \
+                -f $OUTPUT_CONTAINER \
                 \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\""
             echo -e $COMMAND | tee -a "$JOBENCODER" 1> /dev/null
 
             # cover job (set final filename here too)
-            COMMAND="echo \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\" && \
-                ffmpeg $FFMPEG_LOGLEVEL -i \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\" -i \"$COVERIMG\" -c copy -map 0 -map 1 -metadata:s:v title=\"Album cover\" -metadata:s:v comment=\"Cover (Front)\" \"$WORKPATH/$OUTPUT_EXT/${OUTPUT_FINAL/"\$"/"\\\$"}\" && rm \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\""
+            COMMAND="echo \"Setting cover for $WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\" && \
+                ffmpeg $FFMPEG_LOGLEVEL -i \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\" -i \"$COVERIMG\" \
+                -c copy -map 0 -map 1 \
+                -metadata:s:v title=\"Album cover\" \
+                -metadata:s:v comment=\"Cover (Front)\" \
+                \"$WORKPATH/$OUTPUT_EXT/${OUTPUT_FINAL/"\$"/"\\\$"}\" && \
+                rm \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\""
             echo -e $COMMAND | tee -a "$JOBCOVER" 1> /dev/null
-
 
             # m3u line
             BEGSECS=$( echo "${START_TIME[$i]}" | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }' )
             ENDSECS=$( echo "${END_TIME[$i]}" | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }' )
             LENGTH=$( echo "scale=0;($ENDSECS-$BEGSECS+0.5)/1" | bc )
             echo "#EXTINF: $LENGTH, $FSBOOKTITLE - ${TITLE[$i]}" | tee -a "$PLAYLIST" 1> /dev/null
-            echo "$OUTPUT_FINAL" | tee -a "$PLAYLIST" 1> /dev/null
+            echo "${OUTPUT_FINAL/"\$"/"\\\$"}" | tee -a "$PLAYLIST" 1> /dev/null
         done
         
         echo -e "- Encoding:"
@@ -243,22 +280,60 @@ for INPUT_FILE in "${INPUT_FILES[@]}"; do
         if [[ "$DRYRUN" == 1 ]]; then
             echo -e " Or Not! --dryrun specified, nothing to do."
         fi
+    else
+        echo "- Preparing $OUTPUT_EXT Encoding Job"
+        mkdir "$WORKPATH/$OUTPUT_EXT"
+
+        let TRACKNO=1
+        OUTPUT_ENCODE="_$TRACKNO.$OUTPUT_EXT"
+        OUTPUT_FINAL="$FSBOOKTITLE - ${TITLE[$i]}.$OUTPUT_EXT"
+        COMMAND="set -x; echo \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\" && \
+            ffmpeg $FFMPEG_LOGLEVEL -i \"${DRMFREE/"\$"/"\\\$"}\" -vn \
+            -map_chapters -1 \
+            -id3v2_version 4 \
+            -metadata album=\"$BOOKTITLE\" \
+            -metadata genre=\"$GENRE\" \
+            -metadata artist=\"$AUTHOR\" \
+            -metadata album_artist=\"$AUTHOR\" \
+            -metadata date=\"$YEAR\" \
+            -metadata Comment=\"$COMMENT\" \
+            -metadata album-sort=\"$ALBUM_SORT\"
+            -metadata artist-sort=\"$ARTIST_SORT\"
+            -codec:a $OUTPUT_CODEC \
+            -b:a \"${BITRATE_KBPS}k\" \
+            -f $OUTPUT_CONTAINER \
+            \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\""
+            echo -e $COMMAND | tee -a "$JOBENCODER" 1> /dev/null
+
+        # cover job (set final filename here too)
+        COMMAND="echo \"Setting cover for $WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\" && \
+            ffmpeg $FFMPEG_LOGLEVEL -i \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\" -i \"$COVERIMG\" \
+            -c copy -map 0 -map 1 \
+            -metadata:s:v title=\"Album cover\" \
+            -metadata:s:v comment=\"Cover (Front)\" \
+            \"$WORKPATH/$OUTPUT_EXT/${OUTPUT_FINAL/"\$"/"\\\$"}\" && \
+            rm \"$WORKPATH/$OUTPUT_EXT/$OUTPUT_ENCODE\""
+        echo -e $COMMAND | tee -a "$JOBCOVER" 1> /dev/null
+
+        echo -e "- Encoding:"
+        if [[ "$DRYRUN" == 0 ]]; then
+            (exec "$JOBENCODER")
+            (exec "$JOBCOVER")
+        fi
+        if [[ "$DRYRUN" == 1 ]]; then
+            echo -e " Or Not! --dryrun specified, nothing to do."
+        fi
+
     fi
 
     # clean up
     rm "$JOBENCODER"
     rm "$JOBCOVER"
     if [[ "$DRYRUN" == 0 ]]; then
-        mkdir "./$FSBOOKTITLE" -p
-        cp $COVERIMG "./$FSBOOKTITLE/" -f
-        if [[ "$OUTPUT_FLAC" == 1 ]]; then
-            mkdir "./$FSBOOKTITLE/m4b" -p
-            cp $WORKPATH/flac/* "./$FSBOOKTITLE/flac/" -f
-        fi
-
-        if [[ -z "$OUTPUT_EXT" ]]; then
-            mkdir "$(dirname "$INPUT_FILE")/$FSBOOKTITLE/$OUTPUT_EXT/" -p
-            cp $WORKPATH/$OUTPUT_EXT/* "$(dirname "$INPUT_FILE")/$FSBOOKTITLE/$OUTPUT_EXT/" -f
+        if [[ -n "$OUTPUT_EXT" ]]; then
+            mkdir "$(dirname "$INPUT_FILE")/$OUTPUT_EXT/" -p
+            cp $COVERIMG "$(dirname "$INPUT_FILE")/$OUTPUT_EXT/" -f
+            cp $WORKPATH/$OUTPUT_EXT/* "$(dirname "$INPUT_FILE")/$OUTPUT_EXT/" -f
         fi
     fi
     rm -r "$WORKPATH"
@@ -267,7 +342,7 @@ for INPUT_FILE in "${INPUT_FILES[@]}"; do
     echo -e "- Done. processed in $(($SPLIT_RUN / 3600))hrs $((($SPLIT_RUN / 60) % 60))min $(($SPLIT_RUN % 60))sec.\n"
 done
 
-#total time if more than one
+# total time if more than one
 if [[ ${#INPUT_FILES[@]} -gt "1" ]]; then
     echo -e "\nDone processing ${#INPUT_FILES[@]} file(s) in $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec."
 fi
